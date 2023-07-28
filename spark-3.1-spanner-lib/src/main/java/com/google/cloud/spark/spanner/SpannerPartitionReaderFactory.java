7b7122c7b929e348433fac8f00384951ff61e70d
Emmanuel T Odeke <emmanuel@orijtech.com>
2023-07-27 22:05:03 -0700

    Implement more interfaces

---
+++ spark-3.1-spanner-lib/src/main/java/com/google/cloud/spark/spanner/SpannerPartitionReaderFactory.java
@@ -0,0 +1,45 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.cloud.spark.spanner;
+
+import org.apache.spark.sql.catalyst.InternalRow;
+import org.apache.spark.sql.connector.read.Batch;
+import org.apache.spark.sql.connector.read.ColumnarBatch;
+import org.apache.spark.sql.connector.read.InputPartition;
+import org.apache.spark.sql.connector.read.Partition;
+import org.apache.spark.sql.connector.read.PartitionReader;
+import org.apache.spark.sql.connector.read.Scan;
+import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.util.CaseInsensitiveStringMap;
+
+public class SpannerPartitionFactory implements PartitionReaderFactory {
+  @Override
+  public PartititionReader<ColumnarBatch> createColumnarReader(InputPartition partition) {
+    // TODO: Fill me in.
+    return null;
+  }
+
+  @Override
+  public PartitionReader<InternalRow> createReader(InputPartition partition) {
+    // TODO: Fill me in.
+    return null;
+  }
+
+  @Override
+  public boolean supportsColumnarReads(InputPartition partition) {
+    // TODO: Fill me in.
+    return false;
+  }
+}

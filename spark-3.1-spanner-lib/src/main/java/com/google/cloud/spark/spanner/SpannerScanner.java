9baf07639ce4e2d33ac9ced05508c0b6eaea9dc8
Emmanuel T Odeke <emmanuel@orijtech.com>
2023-08-23 21:07:00 +0300

    bring back .Batch to SpannerScanner

--- spark-3.1-spanner-lib/src/main/java/com/google/cloud/spark/spanner/SpannerScanner.java
+++ spark-3.1-spanner-lib/src/main/java/com/google/cloud/spark/spanner/SpannerScanner.java
@@ -14,24 +14,71 @@
 
 package com.google.cloud.spark.spanner;
 
+import com.google.cloud.spanner.BatchClient;
+import com.google.cloud.spanner.BatchReadOnlyTransaction;
+import com.google.cloud.spanner.Options;
+import com.google.cloud.spanner.PartitionOptions;
+import com.google.cloud.spanner.Statement;
+import com.google.cloud.spanner.TimestampBound;
+import com.google.common.collect.Streams;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.spark.Partition;
+import org.apache.spark.sql.connector.read.Batch;
+import org.apache.spark.sql.connector.read.InputPartition;
+import org.apache.spark.sql.connector.read.PartitionReaderFactory;
 import org.apache.spark.sql.connector.read.Scan;
 import org.apache.spark.sql.types.StructType;
 
 /*
  * SpannerScanner implements Scan.
  */
-public class SpannerScanner implements Scan {
+public class SpannerScanner implements Batch, Scan {
   private SpannerTable spannerTable;
   private Map<String, String> opts;
+  private BatchClient batchClient;
 
   public SpannerScanner(Map<String, String> opts) {
     this.opts = opts;
     this.spannerTable = new SpannerTable(null, opts);
+    this.batchClient = SpannerUtils.batchClientFromProperties(this.opts);
   }
 
   @Override
   public StructType readSchema() {
     return this.spannerTable.schema();
+  }
+
+  @Override
+  public Batch toBatch() {
+    return this;
+  }
+
+  @Override
+  public PartitionReaderFactory createReaderFactory() {
+    return new SpannerPartitionReaderFactory(this.opts);
+  }
+
+  @Override
+  public InputPartition[] planInputPartitions() {
+    // TODO: Receive the columns and filters that were pushed down.
+    String sqlStmt = "SELECT * FROM " + this.spannerTable.name();
+    try (BatchReadOnlyTransaction txn =
+        this.batchClient.batchReadOnlyTransaction(TimestampBound.strong())) {
+      List<com.google.cloud.spanner.Partition> rawPartitions =
+          txn.partitionQuery(
+              PartitionOptions.getDefaultInstance(),
+              Statement.of(sqlStmt),
+              Options.dataBoostEnabled(true));
+
+      List<Partition> parts =
+          Streams.mapWithIndex(
+                  rawPartitions.stream(),
+                  (part, index) -> new SpannerPartition(part, Math.toIntExact(index)))
+              .collect(Collectors.toList());
+
+      return parts.toArray(new InputPartition[0]);
+    }
   }
 }

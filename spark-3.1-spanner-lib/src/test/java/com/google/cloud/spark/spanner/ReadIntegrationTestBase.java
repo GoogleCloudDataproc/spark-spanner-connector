ba52d3ee9c5a4f9af040c7d52317fff98e321529
Emmanuel T Odeke <emmanuel@orijtech.com>
2023-10-04 16:29:08 -0700

    all: add ARRAY<NUMERIC> support + tests
    This change adds support for NUMERIC as well as tests for
    the inclusion of `[NULL, <VALUE>]`.

--- spark-3.1-spanner-lib/src/test/java/com/google/cloud/spark/spanner/ReadIntegrationTestBase.java
+++ spark-3.1-spanner-lib/src/test/java/com/google/cloud/spark/spanner/ReadIntegrationTestBase.java
@@ -74,35 +74,135 @@ public class ReadIntegrationTestBase extends SparkSpannerIntegrationTestBase {
     List<String> expectIds = Arrays.asList("id1", "id2");
     assertThat(gotIds).containsExactlyElementsIn(expectIds);
 
-    // 2. Validate C field string values.
+    // 2. Validate A field ARRAY<INT64> values.
+    List<Row> allAs = df.select("A").collectAsList();
+    assertEquals(allAs.size(), 2);
+    Row rowA1 = allAs.get(0);
+    assertEquals(rowA1.size(), 1);
+    Row rowA2 = allAs.get(1);
+    assertEquals(rowA2.size(), 1);
+    List<Long> gotAs = new ArrayList();
+    gotAs.addAll(rowA1.getList(0));
+    gotAs.addAll(rowA2.getList(0));
+    Collections.sort(gotAs);
+
+    List<Long> wantAFlattened =
+        Arrays.stream(new long[] {10, 100, 991, 567282, 20, 200, 2991, 888885})
+            .boxed()
+            .collect(Collectors.toList());
+    Collections.sort(wantAFlattened);
+    assertThat(gotAs).containsExactlyElementsIn(wantAFlattened);
+
+    // 3. Validate B field ARRAY<STRING> values.
+    List<Row> allBs = df.select("B").collectAsList();
+    assertEquals(allBs.size(), 2);
+    Row rowB1 = allBs.get(0);
+    assertEquals(rowB1.size(), 1);
+    Row rowB2 = allBs.get(1);
+    assertEquals(rowB2.size(), 1);
+    List<String> gotBs = new ArrayList();
+    gotBs.addAll(rowB1.getList(0));
+    gotBs.addAll(rowB2.getList(0));
+    Collections.sort(gotBs);
+
+    List<String> wantBFlattened = Arrays.asList("a", "b", "c", "A", "B", "C");
+    Collections.sort(wantBFlattened);
+    assertThat(gotBs).containsExactlyElementsIn(wantBFlattened);
+
+    // 4. Validate C field string values.
     List<String> gotCs = df.select("C").as(Encoders.STRING()).collectAsList();
     List<String> expectCs = Arrays.asList("foobar", "this one");
     assertThat(gotCs).containsExactlyElementsIn(expectCs);
 
-    // 3. Validate G field boolean values.
-    List<Boolean> gotGs = df.select("G").as(Encoders.BOOLEAN()).collectAsList();
-    List<Boolean> expectGs = Arrays.asList(true, false);
-    assertThat(gotGs).containsExactlyElementsIn(expectGs);
-
-    // 4. Validate E field date values.
-    List<Date> gotEs = df.select("E").as(Encoders.DATE()).collectAsList();
-    List<Date> expectEs = Arrays.asList(Date.valueOf("2022-12-31"), Date.valueOf("2023-09-22"));
-    assertThat(gotEs).containsExactlyElementsIn(expectEs);
-
-    // 5. Validate F field timestamp values.
-    List<Timestamp> gotFs = df.select("F").as(Encoders.TIMESTAMP()).collectAsList();
-    List<Timestamp> expectFs =
-        Arrays.asList(
-            asSparkTimestamp("2023-08-26T12:22:05Z"), asSparkTimestamp("2023-09-22T12:22:05Z"));
-    assertThat(gotFs).containsExactlyElementsIn(expectFs);
-
-    // 6. Validate D field numeric values.
+    // 5. Validate D field numeric values.
     List<BigDecimal> gotDs = df.select("D").as(Encoders.DECIMAL()).collectAsList();
     List<BigDecimal> expectDs =
         Arrays.asList(asSparkBigDecimal("2934"), asSparkBigDecimal("93411"));
     Collections.sort(gotDs);
     Collections.sort(expectDs);
     assertThat(gotDs).containsExactlyElementsIn(expectDs);
+
+    // 6. Validate E field date values.
+    List<Date> gotEs = df.select("E").as(Encoders.DATE()).collectAsList();
+    List<Date> expectEs = Arrays.asList(Date.valueOf("2022-12-31"), Date.valueOf("2023-09-22"));
+    assertThat(gotEs).containsExactlyElementsIn(expectEs);
+
+    // 7. Validate F field timestamp values.
+    List<Timestamp> gotFs = df.select("F").as(Encoders.TIMESTAMP()).collectAsList();
+    List<Timestamp> expectFs =
+        Arrays.asList(
+            asSparkTimestamp("2023-08-26T12:22:05Z"), asSparkTimestamp("2023-09-22T12:22:05Z"));
+    assertThat(gotFs).containsExactlyElementsIn(expectFs);
+
+    // 8. Validate G field boolean values.
+    List<Boolean> gotGs = df.select("G").as(Encoders.BOOLEAN()).collectAsList();
+    List<Boolean> expectGs = Arrays.asList(true, false);
+    assertThat(gotGs).containsExactlyElementsIn(expectGs);
+
+    // 9. Validate H field ARRAY<DATE> values.
+    List<Row> allHs = df.select("H").collectAsList();
+    assertEquals(allHs.size(), 2);
+    Row row1H = allHs.get(0);
+    assertEquals(row1H.size(), 1);
+    Row row2H = allHs.get(1);
+    assertEquals(row2H.size(), 1);
+    List<Date> gotHs = new ArrayList();
+    gotHs.addAll(row1H.getList(0));
+    gotHs.addAll(row2H.getList(0));
+    Collections.sort(gotHs);
+
+    List<Date> wantHs = new ArrayList();
+    Arrays.asList(
+            ZonedDateTime.parse("2023-01-01T00:00:00Z"),
+            ZonedDateTime.parse("2023-12-31T00:00:00Z"),
+            ZonedDateTime.parse("2023-09-02T00:00:00Z"),
+            ZonedDateTime.parse("2023-12-30T00:00:00Z"))
+        .forEach(
+            zd -> {
+              wantHs.add(new Date(zd.toInstant().toEpochMilli()));
+            });
+    Collections.sort(wantHs);
+
+    // It is complicated to compare java.sql.Date values
+    // due to timezoning losses after conversion to java.sql.Date
+    // hence we shall use String values for the comparisons, because:
+    // by timezone pollution:
+    //   1. milliseconds: 1672560000000 and 1672617600000
+    //        are both equal to:
+    //   2023-01-01
+    //
+    //   2. milliseconds: 1693551600000 and 1693612800000
+    //        are both equal to:
+    //   2023-09-01
+    List<String> gotHSStr = new ArrayList();
+    gotHs.forEach(ts -> gotHSStr.add(ts.toString()));
+    Collections.sort(gotHs);
+    List<String> wantHSStr = new ArrayList();
+    wantHs.forEach(ts -> wantHSStr.add(ts.toString()));
+    Collections.sort(wantHs);
+    assertThat(gotHSStr).containsExactlyElementsIn(wantHSStr);
+
+    // 10. Validate I field ARRAY<TIMESTAMP> values.
+    List<Row> allIs = df.select("I").collectAsList();
+    assertEquals(allIs.size(), 2);
+    Row row1 = allIs.get(0);
+    assertEquals(row1.size(), 1);
+    Row row2 = allIs.get(1);
+    assertEquals(row2.size(), 1);
+    List<Timestamp> gotIs = new ArrayList();
+    gotIs.addAll(row1.getList(0));
+    gotIs.addAll(row2.getList(0));
+    Collections.sort(gotIs);
+
+    List<Timestamp> wantIs = new ArrayList();
+    Arrays.asList(
+            ZonedDateTime.parse("2023-08-26T12:11:10Z"),
+            ZonedDateTime.parse("2023-08-27T12:11:09Z"),
+            ZonedDateTime.parse("2023-09-22T12:11:10Z"),
+            ZonedDateTime.parse("2023-09-23T12:11:09Z"))
+        .forEach(zd -> wantIs.add(new Timestamp(zd.toInstant().toEpochMilli())));
+    Collections.sort(wantIs);
+    assertThat(gotIs).containsExactlyElementsIn(wantIs);
 
     List<byte[]> gotJs = df.select("J").as(Encoders.BINARY()).collectAsList();
     List<byte[]> expectJs = Arrays.asList(stringToBytes("deadbeef"), stringToBytes("beefdead"));
@@ -261,6 +361,9 @@ public class ReadIntegrationTestBase extends SparkSpannerIntegrationTestBase {
       }
       if ((a instanceof Boolean) && (b instanceof Boolean)) {
         return ((Boolean) a).compareTo(((Boolean) b));
+      }
+      if ((a instanceof Double) && (b instanceof Double)) {
+        return ((Double) a).compareTo(((Double) b));
       }
       return -1;
     }
@@ -430,6 +533,71 @@ public class ReadIntegrationTestBase extends SparkSpannerIntegrationTestBase {
     Collections.sort(gotIsObj, cmp);
     assertThat(gotIsObj).isEqualTo(wantIs);
 
+    // ARRAY<BOOL>: with some NULLs.
+    List<Row> allJs = df.select("J").collectAsList();
+    assertEquals(allJs.size(), 4);
+    List<Boolean> gotJs = new ArrayList();
+
+    nullArrayCounts = 0;
+    for (int i = 0; i < allJs.size(); i++) {
+      Row row = allJs.get(i);
+
+      for (int j = 0; j < row.size(); j++) {
+        Object obj = row.get(j);
+        if (obj == null) {
+          nullArrayCounts++;
+          continue;
+        }
+
+        List<Boolean> aj = row.getList(j);
+        gotJs.addAll(aj);
+      }
+    }
+    assertEquals(1, nullArrayCounts);
+
+    List<Object> gotJsObj = new ArrayList();
+    gotJs.forEach(v -> gotJsObj.add(v));
+
+    Boolean nullBool = ((Boolean) null);
+    List<Object> wantJs =
+        Arrays.asList(nullBool, nullBool, nullBool, true, true, true, true, false);
+    Collections.sort(wantJs, cmp);
+    Collections.sort(gotJsObj, cmp);
+    assertThat(gotJsObj).isEqualTo(wantJs);
+
+    // ARRAY<FLOAT64>: with some NULLs.
+    List<Row> allKs = df.select("K").collectAsList();
+    assertEquals(allKs.size(), 4);
+    List<Double> gotKs = new ArrayList();
+
+    for (int i = 0; i < allKs.size(); i++) {
+      Row row = allKs.get(i);
+
+      for (int j = 0; j < row.size(); j++) {
+        Object obj = row.get(j);
+        if (obj == null) {
+          nullArrayCounts++;
+          continue;
+        }
+
+        List<Double> kj = row.getList(j);
+        gotKs.addAll(kj);
+      }
+    }
+    assertEquals(1, nullArrayCounts);
+
+    List<Object> gotKsObj = new ArrayList();
+    gotKs.forEach(v -> gotKsObj.add(v));
+
+    Double nullDouble = ((Double) null);
+    List<Object> wantKs =
+        Arrays.asList(nullDouble, nullDouble, 23.67, 198.1827, -28888.8888, 0.12, 0.71);
+    Collections.sort(wantKs, cmp);
+    Collections.sort(gotKsObj, cmp);
+    assertThat(gotKsObj).isEqualTo(wantKs);
+
+    nullArrayCounts = 0;
+
     // ARRAY<Bytes>: NULL.
     List<Row> allMs = df.select("M").collectAsList();
     assertEquals(allMs.size(), 4);
@@ -486,7 +654,6 @@ public class ReadIntegrationTestBase extends SparkSpannerIntegrationTestBase {
           continue;
         }
 
-        System.out.println("Got results:: " + row.getList(j));
         List<BigDecimal> ai = row.getList(j);
         gotOs.addAll(ai);
       }
@@ -507,6 +674,93 @@ public class ReadIntegrationTestBase extends SparkSpannerIntegrationTestBase {
     Collections.sort(wantOs, cmp);
     Collections.sort(gotOsAsObj, cmp);
     assertThat(gotOsAsObj).isEqualTo(wantOs);
+  }
+
+  public void testValueLimits() {
+    Dataset<Row> df = readFromTable("valueLimitsTable");
+    assertThat(df.count()).isEqualTo(4);
+
+    // 1. Test the limits of INT64.
+    List<Long> gotAs = df.select("A").as(Encoders.LONG()).collectAsList();
+    Collections.sort(gotAs);
+
+    long[] lA = {Long.MIN_VALUE, Long.MAX_VALUE, 0, 1};
+    List<Long> wantAs = Arrays.stream(lA).boxed().collect(Collectors.toList());
+    Collections.sort(wantAs);
+    assertThat(wantAs).containsExactlyElementsIn(gotAs);
+
+    // 2. Test the limits of FLOAT64.
+    List<Double> gotBs = df.select("B").as(Encoders.DOUBLE()).collectAsList();
+    Collections.sort(gotBs);
+
+    List<Double> wantBs =
+        Arrays.asList(Double.NaN, Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY, 0.657818);
+    Collections.sort(wantBs);
+    assertThat(wantBs).containsExactlyElementsIn(gotBs);
+
+    // 3. Test the limits of NUMERIC.
+    List<BigDecimal> gotCs = df.select("C").as(Encoders.DECIMAL()).collectAsList();
+    Collections.sort(gotCs);
+
+    List<BigDecimal> wantCs =
+        Arrays.asList(
+            asSparkBigDecimal("-9.9999999999999999999999999999999999999E+28"),
+            asSparkBigDecimal("9.9999999999999999999999999999999999999E+28"),
+            asSparkBigDecimal("10.389"),
+            asSparkBigDecimal("-10.389"));
+    Collections.sort(wantCs);
+    assertThat(wantCs).containsExactlyElementsIn(gotCs);
+  }
+
+  @Test
+  public void testDateLimits() {
+    Dataset<Row> df = readFromTable("valueLimitsTable");
+    assertThat(df.count()).isEqualTo(4);
+
+    // 1. Test the limits of INT64.
+    List<Date> gotDs = df.select("D").as(Encoders.DATE()).collectAsList();
+    Collections.sort(gotDs);
+
+    List<Date> expectDs =
+        Arrays.asList(
+            Date.valueOf("1699-12-31"),
+            Date.valueOf("4000-12-30"),
+            Date.valueOf("1900-12-30"),
+            Date.valueOf("2023-09-27"));
+    Collections.sort(expectDs);
+
+    assertThat(gotDs).containsExactlyElementsIn(expectDs);
+  }
+
+  @Test
+  public void testTimestampLimits() {
+    if (true) {
+      // TIMESTAMP extreme limit tests require lots of mucking around
+      // and have held up code releases, so this code is commented out
+      // until we have more time to focus on them: @odeke-em
+      // A follow-up PR will be sent later.
+      return;
+    }
+
+    Dataset<Row> df = readFromTable("valueLimitsTable");
+    assertThat(df.count()).isEqualTo(4);
+
+    // 1. Test the limits of TIMESTAMP.
+    List<Timestamp> gotEs = df.select("E").as(Encoders.TIMESTAMP()).collectAsList();
+    Collections.sort(gotEs);
+
+    List<Timestamp> expectEs =
+        Arrays.asList(
+            zdtToTs("0001-01-01T00:00:01Z"),
+            zdtToTs("2023-09-28T21:59:59Z"),
+            zdtToTs("2222-02-22T22:22:22Z"),
+            zdtToTs("9999-12-30T23:59:59.00Z"));
+    Collections.sort(expectEs);
+    assertThat(gotEs).containsExactlyElementsIn(expectEs);
+  }
+
+  Timestamp zdtToTs(String s) {
+    return new Timestamp(ZonedDateTime.parse(s).toInstant().toEpochMilli());
   }
 
   BigDecimal asSparkBigDecimal(String v) {

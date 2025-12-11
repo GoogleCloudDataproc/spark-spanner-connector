fb77c462900c7e82da2d0f9ac542da95764fe357
Max Ksyunz <max.ksyunz@improving.com>
2025-12-11 15:37:58 -0800

    Add logging.
    Signed-off-by: Max Ksyunz <max.ksyunz@improving.com>

--- spark-3.1-spanner-lib/src/main/java/com/google/cloud/spark/spanner/SpannerBatchWrite.java
+++ spark-3.1-spanner-lib/src/main/java/com/google/cloud/spark/spanner/SpannerBatchWrite.java
@@ -5,12 +5,17 @@ import org.apache.spark.sql.connector.write.DataWriterFactory;
 import org.apache.spark.sql.connector.write.LogicalWriteInfo;
 import org.apache.spark.sql.connector.write.PhysicalWriteInfo;
 import org.apache.spark.sql.connector.write.WriterCommitMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SpannerBatchWrite implements BatchWrite {
+  private static final Logger log = LoggerFactory.getLogger(SpannerBatchWrite.class);
+
   private final LogicalWriteInfo info;
 
   public SpannerBatchWrite(LogicalWriteInfo info) {
     this.info = info;
+    log.info("Creating SpannerBatchWrite for queryId {}", info.queryId());
   }
 
   @Override
@@ -21,11 +26,12 @@ public class SpannerBatchWrite implements BatchWrite {
 
   @Override
   public void commit(WriterCommitMessage[] messages) {
-    // Commit the write operation.
+    // This is a no-op. There is no per-batch resources allocated.
+    log.info("Committing SpannerBatchWrite for queryId {}", info.queryId());
   }
 
   @Override
   public void abort(WriterCommitMessage[] messages) {
-    // Abort the write operation.
+    log.info("Aborting SpannerBatchWrite for queryId {}", info.queryId());
   }
 }

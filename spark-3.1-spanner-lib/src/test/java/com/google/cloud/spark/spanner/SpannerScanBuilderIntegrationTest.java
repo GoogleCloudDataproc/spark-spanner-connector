1ff4a5bdc2cf8b64fe23218485f5edfd5041fdb2
stevelordbq <steve.lord@improving.com>
2026-01-28 10:38:02 -0800

    Rename integration tests to run in correct maven goal

--- spark-3.1-spanner-lib/src/test/java/com/google/cloud/spark/spanner/SpannerScanBuilderIntegrationTest.java
+++ spark-3.1-spanner-lib/src/test/java/com/google/cloud/spark/spanner/SpannerScanBuilderIntegrationTest.java
@@ -42,7 +42,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class SpannerScanBuilderTest extends SpannerTestBase {
+public class SpannerScanBuilderIntegrationTest extends SpannerTestBase {
 
   @Test
   public void readSchemaShouldWorkInSpannerScanBuilder() throws Exception {
@@ -74,8 +74,8 @@ public class SpannerScanBuilderTest extends SpannerTestBase {
 
   @Test
   public void readSchemaShouldWorkInSpannerScanBuilderForPg() throws Exception {
-    if (SpannerScanBuilderTest.emulatorHost != null
-        && !SpannerScanBuilderTest.emulatorHost.isEmpty()) {
+    if (SpannerScanBuilderIntegrationTest.emulatorHost != null
+        && !SpannerScanBuilderIntegrationTest.emulatorHost.isEmpty()) {
       // Spanner emulator doesn't support the PostgreSql dialect interface.
       // If the emulator is set. We return immediately here.
       // TODO: Use logger instead of System out once logger configuration is set.
@@ -190,7 +190,8 @@ public class SpannerScanBuilderTest extends SpannerTestBase {
 
   @Test
   public void planInputPartitionsShouldSucceedInSpannerScanBuilderPg() throws Exception {
-    if (SpannerTableTest.emulatorHost != null && !SpannerTableTest.emulatorHost.isEmpty()) {
+    if (SpannerTableIntegrationTest.emulatorHost != null
+        && !SpannerTableIntegrationTest.emulatorHost.isEmpty()) {
       // Spanner emulator doesn't support the PostgreSql dialect interface.
       // If the emulator is set. We return immediately here.
       // TODO: Use logger instead of System out once logger configuration is set.

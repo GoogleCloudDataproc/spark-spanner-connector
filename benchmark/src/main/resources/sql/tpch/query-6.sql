20e0b49c4ae34bb4f4a1be222e4ed3ea22c1a3a3
stevelordbq <steve.lord@improving.com>
2026-04-29 09:29:32 -0700

    TPC-H benchmarking update Q6 to remove predicate pushdown so result matches Spanner Studio value

--- benchmark/src/main/resources/sql/tpch/query-6.sql
+++ benchmark/src/main/resources/sql/tpch/query-6.sql
@@ -5,5 +5,5 @@ FROM
 WHERE
     l_shipdate >= DATE '1997-01-01'
   AND l_shipdate < DATE '1998-01-01'
-  AND l_discount BETWEEN 0.06 - 0.01 AND 0.06 + 0.01
+  AND l_discount BETWEEN 0.05 AND 0.07
   AND l_quantity < 25

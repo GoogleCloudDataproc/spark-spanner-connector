a39d27e6033284a40550aee45ea143b7e8f9b5f5
stevelordbq <steve.lord@improving.com>
2026-07-03 14:38:51 -0700

    Upgrade to Spark 4.1

---
+++ spark-4.1-spanner-lib/src/test/java/com/google/cloud/spark/spanner/Spark41SpannerDataWriterTest.java
@@ -0,0 +1,46 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.cloud.spark.spanner;
+
+import org.apache.spark.sql.Encoder;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.catalyst.CatalystTypeConverters;
+import org.apache.spark.sql.catalyst.InternalRow;
+import org.apache.spark.sql.types.StructType;
+
+public class Spark41SpannerDataWriterTest extends SpannerDataWriterTestBase {
+
+  private scala.Function1<Object, Object> converter;
+
+  @Override
+  protected void localSetup() {
+    converter = CatalystTypeConverters.createToCatalystConverter(schema);
+  }
+
+  @Override
+  protected Encoder<Row> getEncoder(StructType schema) {
+    // In Spark 4.1, this returns an AgnosticEncoder which implements Encoder
+    return Encoders.row(schema);
+  }
+
+  @Override
+  protected InternalRow CreateInternalRow(long i) {
+    Row row = RowFactory.create(i, "row" + i);
+
+    return (InternalRow) converter.apply(row);
+  }
+}
